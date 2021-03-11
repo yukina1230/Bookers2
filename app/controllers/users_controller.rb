18f2487348f55abc31bd7c5cef6a92a1273860a3
EC2 Default User <ec2-user@ip-172-31-30-245.ec2.internal>
2021-03-11 09:29:40 +0000

    [Finish]

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
+  before_action :reject_user, only: [:edit]
 
   def show
     @user = User.find(params[:id])
@@ -14,12 +15,7 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = User.find(current_user.id)
-    if @user == current_user
-      render "edit"
-    else
-      redirect_to user_path
-    end
+    @user = User.find(params[:id])
   end
 
   def update
@@ -36,5 +32,12 @@ class UsersController < ApplicationController
 
   def user_params
     params.require(:user).permit(:name, :introduction, :profile_image)
+  end
+
+  def reject_user
+    @user = User.find(params[:id])
+    if @user != current_user
+      redirect_to user_path(current_user)
+    end
   end
 end
