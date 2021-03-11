6bbb6635b16908c4bba8a6e66e2ce575d0e4a7f9
EC2 Default User <ec2-user@ip-172-31-30-245.ec2.internal>
2021-03-11 03:23:20 +0000

    [Update]message_color

--- db/schema.rb
+++ db/schema.rb
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(version: 2021_03_09_003537) do
     t.string "name"
     t.string "profile_image_id"
     t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["name"], name: "index_users_on_name", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
